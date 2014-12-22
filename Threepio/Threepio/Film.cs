098e5268581e52b79df12acc91d278fcf8d72204
Phil Pursglove <phil@philippursglove.com>
2014-12-22 17:24:16 +0000

    Arrays now deserialising into Lists of Uri

--- Threepio/Threepio/Film.cs
+++ Threepio/Threepio/Film.cs
@@ -12,19 +12,31 @@ namespace Threepio
         public string Director { get; set; }
         public string Producer { get; set; }
         public string Opening_Crawl { get; set; }
-        public IEnumerable<Uri> Characters { get; private set; }
+        [JsonProperty("characters")]
+        public List<Uri> Characters { get; private set; }
+        [JsonProperty("planets")]
+        public List<Uri> Planets { get; private set; }
+        [JsonProperty("species")]
+        public List<Uri> Species { get; private set; }
+        [JsonProperty("starships")]
+        public List<Uri> Starships { get; private set; }
+        [JsonProperty("vehicles")]
+        public List<Uri> Vehicles { get; private set; }
 
         public static Film Get(int id)
         {
             try
             {
-                WebClient client = new WebClient();
-                String data = client.DownloadString(string.Format("{0}/films/{1}/", Settings.RootUrl, id));
+                string data;
+                using (WebClient client = new WebClient())
+                {
+                    data = client.DownloadString(string.Format("{0}/films/{1}/", Settings.RootUrl, id));
+                }
                 TextReader textreader = new StringReader(data);
                 JsonReader reader = new JsonTextReader(textreader);
                 return JsonSerializer.Create().Deserialize<Film>(reader);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return null;
             }
