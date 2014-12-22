f5e94f493c22027333e470fe44819c0e185cd0f8
Phil Pursglove <phil@philippursglove.com>
2014-12-22 16:34:53 +0000

    Added some basic error handling to return null if the WebRequest fails

--- Threepio/Threepio/Film.cs
+++ Threepio/Threepio/Film.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 
@@ -11,14 +12,22 @@ namespace Threepio
         public string Director { get; set; }
         public string Producer { get; set; }
         public string Opening_Crawl { get; set; }
+        public IEnumerable<Uri> Characters { get; private set; }
 
         public static Film Get(int id)
         {
-            WebClient client = new WebClient();
-            String data = client.DownloadString(string.Format("{0}/films/{1}/", Settings.RootUrl, id));
-            TextReader textreader = new StringReader(data);
-            JsonReader reader = new JsonTextReader(textreader);
-            return JsonSerializer.Create().Deserialize<Film>(reader);
+            try
+            {
+                WebClient client = new WebClient();
+                String data = client.DownloadString(string.Format("{0}/films/{1}/", Settings.RootUrl, id));
+                TextReader textreader = new StringReader(data);
+                JsonReader reader = new JsonTextReader(textreader);
+                return JsonSerializer.Create().Deserialize<Film>(reader);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
