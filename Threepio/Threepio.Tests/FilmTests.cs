9c3305e6025ba2858b74ed385ac3bb62e2b532e5
Phil Pursglove <phil@philippursglove.com>
2014-12-22 16:35:18 +0000

    Added some tests :-)

---
+++ Threepio/Threepio.Tests/FilmTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Threepio.Tests
+{
+    [TestFixture]
+    public class FilmTests
+    {
+        [Test]
+        public void Film_1_Returns_A_New_Hope()
+        {
+            Film aNewHope = Film.Get(1);
+
+            Assert.AreEqual(aNewHope.Title, "A New Hope");
+        }
+
+        [Test]
+        public void Film_99_Returns_Nothing()
+        {
+            Film nullFilm = Film.Get(99);
+
+            Assert.IsNull(nullFilm);
+        }
+
+    }
+}
---
+++ Threepio/Threepio.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Threepio.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WSP Group plc")]
+[assembly: AssemblyProduct("Threepio.Tests")]
+[assembly: AssemblyCopyright("Copyright © WSP Group plc 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ded9c9e8-69b0-42c6-a681-732927ffbc2f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
