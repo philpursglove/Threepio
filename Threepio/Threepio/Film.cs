f8507334fb488acf6cf7f3ccb71023fa054fd460
Phil Pursglove <phil@philippursglove.com>
2014-12-22 10:29:29 +0000

    Film refactored to use Item

--- Threepio/Threepio/Film.cs
+++ Threepio/Threepio/Film.cs
@@ -1,14 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Net;
-using Newtonsoft.Json;
 
 namespace Threepio
 {
-    public class Film
+    public class Film : Item
     {
-        public DateTime Created { get; set; }
-        public DateTime Edited { get; set; }
         public string Title { get; set; }
         public string Director { get; set; }
         public string Producer { get; set; }
@@ -16,7 +14,6 @@ namespace Threepio
 
         public static Film Get(int id)
         {
-            //return new Film();
             WebClient client = new WebClient();
             String data = client.DownloadString(string.Format("http://swapi.co/api/films/{0}/", id));
             TextReader textreader = new StringReader(data);
