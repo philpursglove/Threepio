6cef0b96095bf48df7b7b18f26476ecf45ded040
Phil Pursglove <phil@philippursglove.com>
2014-12-22 10:27:38 +0000

    Abstract base class for holding common properties

---
+++ Threepio/Threepio/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Threepio
+{
+    public abstract class Item
+    {
+        public DateTime Created { get; set; }
+        public DateTime Edited { get; set; }
+    }
+}
