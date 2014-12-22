c53fba08fe64a1c60e118494d92671d6636e5e1c
Phil Pursglove <phil@philippursglove.com>
2014-12-22 23:09:45 +0000

    Tests updated

--- Threepio/Threepio.Tests/FilmTests.cs
+++ Threepio/Threepio.Tests/FilmTests.cs
@@ -21,5 +21,7 @@ namespace Threepio.Tests
             Assert.IsNull(nullFilm);
         }
 
+
+
     }
 }
---
+++ Threepio/Threepio.Tests/PlanetTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Threepio.Tests
+{
+    [TestFixture]
+    public class PlanetTests
+    {
+        [Test]
+        public void Planet_1_Returns_Planet()
+        {
+            Planet planet = Planet.Get(1);
+
+            Assert.IsNotNull(planet);
+        }
+
+        [Test]
+        public void Planet_Minus1_Returns_Null()
+        {
+            Planet planet = Planet.Get(-1);
+
+            Assert.IsNull(planet);
+        }
+    }
+}
