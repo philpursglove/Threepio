4409da9f056b5ee656e2f7ba2440bbd906cc26b6
Phil Pursglove <phil@philippursglove.com>
2014-12-24 09:59:39 +0000

    WebCLient creation puled out into a factory

---
+++ Threepio/Threepio/WebClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Threepio
+{
+    public static class WebClientFactory
+    {
+        public static WebClient GetClient()
+        {
+            WebClient client = new WebClient();
+
+            // Have to specify a User-Agent or the server returns http 500
+            client.Headers.Add(HttpRequestHeader.UserAgent, "Threepio .Net library");
+
+            return client;
+        }
+    }
+}
