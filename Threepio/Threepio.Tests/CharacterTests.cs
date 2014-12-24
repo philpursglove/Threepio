a2627854c5cc700d73c4352ccff8fdd34a6f4690
Phil Pursglove <phil@philippursglove.com>
2014-12-24 16:57:34 +0000

    First character tests

---
+++ Threepio/Threepio.Tests/CharacterTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Threepio.Tests
+{
+    [TestFixture]
+    public class CharacterTests
+    {
+        [Test]
+        public void Get_Minus1_Throws_404()
+        {
+            Action act = () => Character.Get(-1);
+
+            act.ShouldThrow<WebException>();
+        }
+
+        [Test]
+        public void GetAll_Returns_Multiple_Characters()
+        {
+            var result = Character.GetAll();
+
+            Assert.IsInstanceOf<List<Character>>(result);
+        }
+    }
+}
