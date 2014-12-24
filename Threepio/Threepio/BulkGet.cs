3134d048d1e90c01f717c6ce1230ace653880fc4
Phil Pursglove <phil@philippursglove.com>
2014-12-24 16:42:55 +0000

    BulkGet<T> for deserialising Json arrays

---
+++ Threepio/Threepio/BulkGet.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Threepio
+{
+    internal class BulkGet<T>
+    {
+        [JsonProperty("results")]
+        public List<T> items { get; set; }
+    }
+}
