8c19722c7522548a26a1cccfb6ae3f4ca3cab0e4
Phil Pursglove <phil@philippursglove.com>
2014-12-26 15:04:07 +0000

    Tweaks to properties to make tests green :-)

--- Threepio/Threepio/Vehicle.cs
+++ Threepio/Threepio/Vehicle.cs
@@ -14,35 +14,26 @@ namespace Threepio
         [JsonProperty("vehicle_class")]
         public string Class { get; set; }
         [JsonProperty("cost_in_credits")]
-        public int Cost { get; set; }
+        public string Cost { get; set; }
         public float Length { get; set; }
         public int Crew { get; set; }
         public int Passengers { get; set; }
-        [JsonProperty("hyperdrive_rating")]
-        public string HyperdriveRating { get; set; }
         [JsonProperty("cargo_capacity")]
-        public int CargoCapacity { get; set; }
+        public string CargoCapacity { get; set; }
 
         public List<Uri> Films { get; set; }
         public List<Uri> Pilots { get; set; }
 
         public static Vehicle Get(int id)
         {
-            try
+            string data;
+            using (WebClient client = WebClientFactory.GetClient())
             {
-                string data;
-                using (WebClient client = WebClientFactory.GetClient())
-                {
-                    data = client.DownloadString(string.Format("{0}/vehicles/{1}/", Settings.RootUrl, id));
-                }
-                TextReader textreader = new StringReader(data);
-                JsonReader reader = new JsonTextReader(textreader);
-                return JsonSerializer.Create().Deserialize<Vehicle>(reader);
+                data = client.DownloadString(string.Format("{0}/vehicles/{1}/", Settings.RootUrl, id));
             }
-            catch (Exception)
-            {
-                return null;
-            }
+            TextReader textreader = new StringReader(data);
+            JsonReader reader = new JsonTextReader(textreader);
+            return JsonSerializer.Create().Deserialize<Vehicle>(reader);
         }
 
         public static List<Vehicle> GetAll(int pageSize = 20, int pageNumber = 1)
