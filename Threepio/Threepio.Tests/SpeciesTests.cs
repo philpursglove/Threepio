a0d8227fd3fa3b9775bf49864963451977f3c173
Phil Pursglove <phil@philippursglove.com>
2014-12-26 14:28:42 +0000

    First tests implemented

--- Threepio/Threepio.Tests/SpeciesTests.cs
+++ Threepio/Threepio.Tests/SpeciesTests.cs
@@ -1,11 +1,28 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Net;
 
 namespace Threepio.Tests
 {
     [TestFixture]
     class SpeciesTests
     {
-        //TODO
+        [Test]
+        public void Species_1_Returns_A_Species()
+        {
+            Species species = Species.Get(1);
+
+            Assert.IsNotNull(species);
+        }
+
+        [Test]
+        public void Species_Minus1_Throws_A_404()
+        {
+            Action act = () => Species.Get(-1);
+
+            act.ShouldThrow<WebException>();
+        }
 
     }
 }
