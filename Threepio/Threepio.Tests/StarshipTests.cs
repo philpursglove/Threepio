6dce8e2772f721cb416d6a77d1ea62e1e4acff8c
Phil Pursglove <phil@philippursglove.com>
2014-12-26 15:03:40 +0000

    Final two TestFixtures added

---
+++ Threepio/Threepio.Tests/StarshipTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Threepio.Tests
+{
+    [TestFixture]
+    class StarshipTests
+    {
+        [Test]
+        public void Starship_10_Returns_A_Starship()
+        {
+            Starship ship = Starship.Get(10);
+
+            Assert.IsNotNull(ship);
+        }
+
+        [Test]
+        public void Starship_Minus1_Throws_A_404()
+        {
+            Action act = () => Starship.Get(-1);
+
+            act.ShouldThrow<WebException>();
+        }
+
+        [Test]
+        public void GetAll_Returns_Multiple_Starships()
+        {
+            var result = Starship.GetAll();
+
+            Assert.IsInstanceOf<List<Starship>>(result);
+        }
+    }
+}
---
+++ Threepio/Threepio.Tests/VehicleTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Threepio.Tests
+{
+    [TestFixture]
+    class VehicleTests
+    {
+        [Test]
+        public void Vehicle_18_Returns_A_Vehicle()
+        {
+            Vehicle vehicle = Vehicle.Get(18);
+
+            Assert.IsNotNull(vehicle);
+        }
+
+        [Test]
+        public void Vehicle_Minus1_Throws_A_404()
+        {
+            Action act = () => Vehicle.Get(-1);
+
+            act.ShouldThrow<WebException>();
+        }
+
+        [Test]
+        public void GetAll_Returns_Multiple_Vehicles()
+        {
+            var result = Vehicle.GetAll();
+
+            Assert.IsInstanceOf<List<Vehicle>>(result);
+        }
+    }
+}
