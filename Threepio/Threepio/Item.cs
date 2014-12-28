5f1680528f061b1eb52aa6a66523a09d78dc044f
Phil Pursglove <phil@philippursglove.com>
2014-12-28 20:38:48 +0000

    ParseLink added

--- Threepio/Threepio/Item.cs
+++ Threepio/Threepio/Item.cs
@@ -6,5 +6,24 @@ namespace Threepio
     {
         public DateTime Created { get; set; }
         public DateTime Edited { get; set; }
+
+        internal static int ParseLink(Uri uri)
+        {
+            string link = uri.AbsoluteUri;
+
+            link = link.Replace(Settings.RootUrl, string.Empty);
+            link = link.Replace("/", string.Empty);
+            link = link.Replace("films", string.Empty);
+            link = link.Replace("people", string.Empty);
+            link = link.Replace("species", string.Empty);
+            link = link.Replace("planets", string.Empty);
+            link = link.Replace("starships", string.Empty);
+            link = link.Replace("vehicles", string.Empty);
+
+            int result;
+            int.TryParse(link, out result);
+
+            return result;
+        }
     }
 }
