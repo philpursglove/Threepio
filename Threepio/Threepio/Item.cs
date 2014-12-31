0d0c9199c0757581acdff6577ce9845d59b44789
Phil Pursglove <phil@philippursglove.com>
2014-12-31 10:56:25 +0000

    List<Uri> pulled up to Item

--- Threepio/Threepio/Item.cs
+++ Threepio/Threepio/Item.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace Threepio
 {
@@ -6,6 +8,26 @@ namespace Threepio
     {
         public DateTime Created { get; set; }
         public DateTime Edited { get; set; }
+
+        [JsonProperty("films")]
+        internal List<Uri> FilmUris { get; set; }
+        [JsonProperty("species")]
+        internal List<Uri> SpeciesUris { get; set; }
+        [JsonProperty("starships")]
+        internal List<Uri> StarshipUris { get; set; }
+        [JsonProperty("vehicles")]
+        internal List<Uri> VehicleUris { get; set; }
+        [JsonProperty("planets")]
+        internal List<Uri> PlanetUris { get; private set; }
+        [JsonProperty("characters")]
+        internal List<Uri> CharacterUris { get; private set; }
+        [JsonProperty("residents")]
+        internal List<Uri> ResidentUris { get; set; }
+        [JsonProperty("people")]
+        internal List<Uri> MemberUris { get; set; }
+        [JsonProperty("pilots")]
+        internal List<Uri> PilotUris { get; set; }
+
 
         internal static int ParseLink(Uri uri)
         {
